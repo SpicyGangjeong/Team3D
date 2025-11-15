946477feaf38f74b9038bd7bdc48a5073f2e3886
kimnuri <nurl0306@naver.com>
2025-11-15 12:07:42 +0900

    [add] assert

--- Jusin154_Team3D/Editor/Private/EditEffect.cpp
+++ Jusin154_Team3D/Editor/Private/EditEffect.cpp
@@ -59,7 +59,7 @@ void CEditEffect::Late_Update(_float fTimeDelta)
 	if (m_pInstance_ModelCom == nullptr)
 		return;
 
-
+	ASSERT_JINHO(false && 이거 고쳐야됨)
 	//{
 	//	if (m_pGameInstance->isIn_WorldFrustum(Get_WorldPostion(), m_pTransformCom->Get_Radius())) {
 	//		m_pGameInstance->Add_RenderGroup(RENDER::BLEND, this);
