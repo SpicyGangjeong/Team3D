4e9abdb1686755c1d5c199d367c4f24ef5b97cd6
kimnuri <nurl0306@naver.com>
2025-11-21 19:56:35 +0900

    [fix] batchfiles

--- Jusin154_Team3D/Update_Shaders.bat
+++ Jusin154_Team3D/Update_Shaders.bat
@@ -1,2 +1,4 @@
 ﻿
-xcopy			/y /s	.\Editor\Bin\Resources\ShaderFiles\*.*					.\Client\Bin\Resources\ShaderFiles\
+robocopy ".\Editor\Bin\Resources\ShaderFiles\" ".\Client\Bin\Resources\ShaderFiles\" /MIR /FFT /NDL /NFL /R:0 /W:0 /ETA
+IF %ERRORLEVEL% LEQ 4 EXIT /B 0
+pause
