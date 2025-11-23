824cbf2661f51f44c6f18ae012883a6cbe870cfa
kimnuri <nurl0306@naver.com>
2025-11-23 21:50:35 +0900

    [add] 카메라 럴프 추가

--- Jusin154_Team3D/Client/Private/Camera_Debug.cpp
+++ Jusin154_Team3D/Client/Private/Camera_Debug.cpp
@@ -21,6 +21,7 @@ void CCamera_Debug::Update(_float fTimeDelta)
 	Transition(fTimeDelta);
 	_float3 vCamPos = {};
 	XMStoreFloat3(&vCamPos, m_pTransformCom->Get_State(STATE::POSITION));
+
 	GUI::Text("Cam Coord %.2f, %.2f, %.2f", vCamPos.x, vCamPos.y, vCamPos.z);
 	if (m_pGameInstance->Key_Up(DIK_GRAVE)) {
 		m_bMovable = !m_bMovable;
