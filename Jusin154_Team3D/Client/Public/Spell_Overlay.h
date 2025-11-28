940db14efe6241f3db3146f1e789f74783a474c4
qordlsgur <127704460+qordlsgur@users.noreply.github.com>
2025-11-28 17:45:01 +0900

    Revert "[Fix] 이미지 및 터지는거 잡음"
    This reverts commit fb648f439979ef5e79453f9804cdf615753e2782.

--- Jusin154_Team3D/Client/Public/Spell_Overlay.h
+++ Jusin154_Team3D/Client/Public/Spell_Overlay.h
@@ -39,7 +39,6 @@ private:
 	CTexture* m_pDiffuse_TextureCom = { nullptr };
 	CTexture* m_pDiffuse_TextureCom1 = { nullptr };
 	CTexture* m_pDiffuse_TextureCom2 = { nullptr };
-	CTexture* m_pDiffuse_TextureCom3 = { nullptr };
 	CShader* m_pShaderCom = { nullptr };
 	CVIBuffer_Rect* m_pVIBufferCom = { nullptr };
 
@@ -54,7 +53,6 @@ public:
 	virtual void Free() override;
 #ifdef _DEBUG
 	void Describe_Entity() override;
-
 #endif // _DEBUG
 };
 
--- Jusin154_Team3D/Editor/Private/HpBarBG.cpp
+++ Jusin154_Team3D/Editor/Private/HpBarBG.cpp
@@ -105,12 +105,12 @@ void CHpBarBG::Update(_float fTimeDelta)
 
 	if (m_pGameInstance->Key_Down(DIK_3))
 	{
-		Lerp_PosX(20.f);
+		Lerp_PosX(100.f);
 	}
 
 	if (m_pGameInstance->Key_Down(DIK_4))
 	{
-		Lerp_PosX(-20.f);
+		Lerp_PosX(-101.f);
 	}
 	if (m_fHpBar <= 0.3f)
 	{
@@ -327,8 +327,6 @@ void CHpBarBG::Free()
 	SAFE_RELEASE(m_pVIBufferCom);
 }
 
-#ifdef _DEBUG
 void CHpBarBG::Describe_Entity()
 {
 }
-#endif // _DEBUG
