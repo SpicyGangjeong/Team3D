0f2a02c90ee5caa071d984df9b13aa0dd177f34a
kimnuri <nurl0306@naver.com>
2026-01-14 23:12:02 +0900

    [add] PhysxFilter 정상화

--- Jusin154_Team3D/Client/Private/Level_GamePlay.cpp
+++ Jusin154_Team3D/Client/Private/Level_GamePlay.cpp
@@ -126,7 +126,7 @@ HRESULT CLevel_GamePlay::Initialize(void* pArg)
 	_bool bLoadNPC = { true };
 #ifdef _DEBUG
 #ifdef 기무리
-	bLoadNPC = true;
+	bLoadNPC = false;
 #endif
 #endif // _DEBUG
 	if (true == bLoadNPC) {
@@ -170,7 +170,7 @@ void CLevel_GamePlay::Update(_float fTimeDelta)
 	_bool bStartCinematic = { true };
 #ifdef _DEBUG
 #ifdef 기무리
-	bStartCinematic = true;
+	bStartCinematic = false;
 #elif 진우
 	bStartCinematic = true;
 #elif Bin
@@ -365,7 +365,7 @@ HRESULT CLevel_GamePlay::Ready_Background()
 #ifdef 기무리
 	isReady_Background = true;
 	isReady_Hogsmeade = true;
-	isReady_Hogwart = true;
+	isReady_Hogwart = false;
 #endif // 
 #ifdef 나
 	isReady_Background = false;
@@ -1140,8 +1140,8 @@ HRESULT CLevel_GamePlay::Ready_Layer_Npc()
 	isLoad_NPC = false;
 #endif // 
 #ifdef 기무리
-	isLoad_NPC = true;
-	isLoad_RandomNPC = true;
+	isLoad_NPC = false;
+	isLoad_RandomNPC = false;
 #endif // 
 #ifdef 나
 	isLoad_RandomNPC = true;
